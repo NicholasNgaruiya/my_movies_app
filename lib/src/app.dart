0ab84d7dc96ebcc623b4a3adb27d5d74146432f1
NicholasNgaruiya <nicholasngaruiya387@gmail.com>
2023-05-05 13:19:46 +0300

    Created the movie app that pulls from the api and also implemented bloc and clean architecture

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'ui/movie_list.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData.dark(),
+        home: const Scaffold(
+          body: MovieList(),
+        ));
+  }
+}
